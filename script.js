556171fdec4464bc9ed1f408c6d3aed0b71a4a1a
teresethulin <terese.thulin@gmail.com>
2019-11-11 14:17:46 +0100

    Add arrow scroll to top function

---
+++ script.js
@@ -0,0 +1,21 @@
+//Get the button
+const myArrow = document.getElementById("arrow");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+        myArrow.style.display = "block";
+    } else {
+        myArrow.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
